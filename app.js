d98ca9e9216721cc2b37468c79f182357b6b2a99
yannick <yannick.xu@epitech.eu>
2020-06-02 16:20:55 +0200

    Add year and gender field

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
-app.set('views', __dirname)
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'ejs')
+app.set('views', __dirname);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.render('index.html')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).send(req.body);
 })
 
 app.listen(8080, () => {
