10526475cf1b004e44caac0095b2eafe108bef13
yannick <yannick.xu@epitech.eu>
2020-06-02 17:04:49 +0200

    script to insert file to dv

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const database = require('./database')
 
 app.set('views', __dirname);
 app.engine('html', require('ejs').renderFile);
@@ -25,6 +26,15 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     console.log(req.body);
     res.status(200).send(req.body);
+})
+
+app.post('/db', (req, res) => {
+    var fs = require('fs');
+    console.log(req.body.file);
+    var text = fs.readFileSync('names/' + req.body.file, 'utf8');
+    console.log(text);
+    database.add(text, req.body.file);
+    res.status(200).send('ok');
 })
 
 app.listen(8080, () => {
---
+++ database.js
@@ -0,0 +1,23 @@
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        user: 'Ton username',
+        password: 'Ton mode de passe',
+        database: 'la db'
+    }
+});
+
+function add(data, filename) {
+    year = Number(filename.substring(3, 7));
+    let datas = data.split('\r\n');
+    datas.forEach(element => {
+        if (element) {
+            value = element.split(',')
+            knex('ta table').insert([{name: value[0]}, {sex: value[1]}, { number: Number(value[2])}])
+        }
+    });
+}
+
+module.exports.knex = knex;
+module.exports.add = add;
